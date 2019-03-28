af1a81a7f1bc2f5bfbe0855039212d26ef2232eb
Luda <luisdavidgd@gmail.com>
2019-03-28 10:40:12 -0500

    Updates

---
+++ precache-manifest.296b95bb0725ae0af9fd59f995abc76a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "829b13c8faac868f1f5b",
+    "url": "/react-sample/static/css/main.63697f62.chunk.css"
+  },
+  {
+    "revision": "829b13c8faac868f1f5b",
+    "url": "/react-sample/static/js/main.d987878f.chunk.js"
+  },
+  {
+    "revision": "885a3f7248e53918799c",
+    "url": "/react-sample/static/js/runtime~main.59592c43.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-sample/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-sample/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "747c1ed595f4a2cb7870ff77e62d2d4e",
+    "url": "/react-sample/index.html"
+  }
+];
