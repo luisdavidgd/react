a02eb29789c5d56e68b86f93d7051cb1f755a93e
Luda <luisdavidgd@gmail.com>
2019-03-29 14:23:37 -0500

    Todos feature added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import Header from './components/layout/Header'
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
 import './App.css';
+export class App extends Component {
+  state = {
+    todos: [
+      { id: Math.random(), title: 'Take out the trash', completed: false },
+      { id: Math.random(), title: 'Dinner with wife', completed: false },
+      { id: Math.random(), title: 'Meeting with boss', completed: false }
+    ]
+  }
 
-class App extends Component {
+  // Toggle Complete
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    });
+  }
+
+  // del todo
+  delTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    })
+  }
+
+  //Add todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: Math.random(),
+      title,
+      completed: false
+    }
+    this.setState({todos: [...this.state.todos, newTodo]})
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload. JUST DO IT
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <Header />
+          <AddTodo
+            addTodo={this.addTodo} />
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            delTodo={this.delTodo} />
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/components/layout/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header style={headerStyle}>
+      <h1>Todo List</h1>
+    </header>
+  )
+}
+
+const headerStyle = {
+    backgroundColor: '#333',
+    color: '#fff',
+    textAlign: 'center',
+}
