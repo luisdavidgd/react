baabf9233fb24b9f9364541beb9455ada5aacdf2
Luis Gallo <lgallo@ctdevelopers.com>
2025-04-18 15:21:16 -0500

    Updates

--- precache-manifest.ede292a5a315419a246dcc36d56a4c5e.js
+++ precache-manifest.ede292a5a315419a246dcc36d56a4c5e.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "a35223dfa6801ff4c7f7",
-    "url": "/react/static/css/main.71831e6e.chunk.css"
+    "revision": "f32854e6675e1c37f892",
+    "url": "/react/static/css/main.641f3d56.chunk.css"
   },
   {
-    "revision": "a35223dfa6801ff4c7f7",
-    "url": "/react/static/js/main.95d47a3e.chunk.js"
+    "revision": "f32854e6675e1c37f892",
+    "url": "/react/static/js/main.b3041936.chunk.js"
   },
   {
     "revision": "4e6a4b677d1c1e6ba359",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/react/static/media/logo.5d5d9eef.svg"
   },
   {
-    "revision": "ed4c630fb0c1697791a8bc704f1707a7",
+    "revision": "dac2ea30da3694acbf21c7633d62b32f",
     "url": "/react/index.html"
   }
 ];
